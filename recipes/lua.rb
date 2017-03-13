d1b9c7db6bc223fab188e6243a58d18a9ba61c2c
Antek Baranski <antek.baranski@gmail.com>
2017-03-13 12:27:35 -0700

    Clean luajit library inclusion by nginx

--- recipes/lua.rb
+++ recipes/lua.rb
@@ -32,10 +32,7 @@ bash 'extract_luajit' do
     mkdir -p #{luajit_extract_path}
     tar xzf #{luajit_src_filename} -C #{luajit_extract_path}
     cd luajit-#{node['nginx']['luajit']['version']}/LuaJIT-#{node['nginx']['luajit']['version']}
-    export LUAJIT_INC="/usr/local/include/luajit-2.0"
-    export LUAJIT_LIB="/usr/local/lib"
     make && make install
-    ldconfig /usr/local/lib
   EOH
   not_if { ::File.exist?(luajit_extract_path) }
 end
@@ -44,3 +41,6 @@ node.run_state['nginx_source_env'].merge!(
   'LUAJIT_INC' => '/usr/local/include/luajit-2.0',
   'LUAJIT_LIB' => '/usr/local/lib'
 )
+
+node.run_state['nginx_configure_flags'] =
+    node.run_state['nginx_configure_flags'] | ['--with-ld-opt="-Wl,-rpath,/usr/local/lib"']
